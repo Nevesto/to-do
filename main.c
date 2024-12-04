8e2612a071ceae4374265205d55461d40482f989
nevest <guilhermejcarlucci@hotmail.com>
2024-12-04 13:45:22 -0300

    better menu, save tasks in files

--- main.c
+++ main.c
@@ -32,14 +32,12 @@ int insert(ListaCDE *l, int id, char *desc, int priority, char *date) {
     strcpy(aux->desc, desc);
     strcpy(aux->date, date);
 
-    l->tam++;
-
     if(l->start == NULL) {
+        aux->prox = aux;
+        aux->ant = aux;
         l->start = aux;
         l->end = aux;
-        aux->prox = l->start;
-        aux->ant = l->end;
-
+        l->tam++;
         return 1;
     }
 
@@ -53,6 +51,7 @@ int insert(ListaCDE *l, int id, char *desc, int priority, char *date) {
 
             if(current == l->start) l->start = aux;
 
+            l->tam++;
             return 1;
         }
 
@@ -62,9 +61,10 @@ int insert(ListaCDE *l, int id, char *desc, int priority, char *date) {
     aux->prox = l->start;
     aux->ant = l->end;
     l->end->prox = aux;
+    l->start->ant = aux;
     l->end = aux;
-    l->start->ant = l->end;
 
+    l->tam++;
     return 1;
 }
 
@@ -76,146 +76,193 @@ int removal(ListaCDE *l, int id) {
         return 0;
     }
 
-    if((id == l->start->id) && (l->start == l->end)) {
-        aux = l->start;
-        l->start = NULL;
-        l->end = NULL;
-        free(aux);
-        l->tam--;
-
-        return 1;
-    }
-
-    if(id == l->start->id) {
-        aux = l->start;
-        l->start = aux->prox;
-        l->start->ant = l->end;
-        l->end->prox = l->start;
-        free(aux);
-        l->tam--;
-
-        return 1;
-    }
-
-    if(id == l->end->id) {
-        aux = l->end;
-        l->end = aux->ant;
-        l->end->prox = l->start;
-        l->start->ant = l->end;
-        free(aux);
-        l->tam--;
-
-        return 1;
-    }
-
-    aux = l->start->prox;
-
-    while((id != aux->id) && (aux != l->end)) {
+    aux = l->start;
+    do {
+        if(aux->id == id) {
+            if(aux == l->start && aux == l->end) {
+                l->start = NULL;
+                l->end = NULL;
+            } else {
+                aux->ant->prox = aux->prox;
+                aux->prox->ant = aux->ant;
+                if(aux == l->start) l->start = aux->prox;
+                if(aux == l->end) l->end = aux->ant;
+            }
+            free(aux);
+            l->tam--;
+            return 1;
+        }
         aux = aux->prox;
-    }
+    } while(aux != l->start);
 
-    if(aux == l->end) {
-        printf("Elemento %d nao encontrado\n", id);
-        return 0;
-    }
-
-    aux->prox->ant = aux->ant;
-    aux->ant->prox = aux->prox;
-    free(aux);
-    l->tam--;
-
-    return 1;
-}
-
-int isEmpty(ListaCDE l) {
-    if(l.start == NULL) return 1;
+    printf("Elemento %d nao encontrado\n", id);
     return 0;
 }
 
-int getFirst(ListaCDE l) {
-    return l.start->id;
+int isEmpty(ListaCDE l) {
+    return l.start == NULL;
 }
 
-int getLast(ListaCDE l) {
-    return l.end->id;
-}
-
-int getSize(ListaCDE l) {
-    return l.tam;
-}
-
-void show(ListaCDE l, int sense) {
+void show(ListaCDE l) {
     Node *aux;
 
     if(l.start == NULL) {
         printf("Lista vazia\n");
         return;
     } else {
-        if(sense == 1) {
-            aux = l.start;
-            do{
-                printf("Mostrando a lista: \n");
-                printf("ID: %d\n", aux->id);
-                printf("Prioridade: %d\n", aux->priority);
-                printf("Descricao: %s\n", aux->desc);
-                printf("Data de entrega: %s\n", aux->date);
-                aux = aux->prox;
-            } while(aux != l.start);
-        } else {
-            aux = l.end;
-            do {
-                printf("Mostrando a lista: \n");
-                printf("ID: %d\n", aux->id);
-                printf("Prioridade: %d\n", aux->priority);
-                printf("Descricao: %s\n", aux->desc);
-                printf("Data de entrega: %s\n", aux->date);
-                aux = aux->ant;
-            } while(aux != l.end);
-        }
+        aux = l.start;
+        do{
+            printf("ID: %d\n", aux->id);
+            printf("Prioridade: %d\n", aux->priority);
+            printf("Descricao: %s\n", aux->desc);
+            printf("Data de entrega: %s\n\n", aux->date);
+            aux = aux->prox;
+        } while(aux != l.start);
     }
+}
+
+void saveToFile(ListaCDE l) {
+    FILE *file = fopen("todo.txt", "w");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo todo.txt para escrita.\n");
+        return;
+    }
+
+    if (l.start == NULL) {
+        fprintf(file, "");
+    } else {
+        Node *aux = l.start;
+        do {
+            fprintf(file, "%d;%d;%s;%s\n", aux->id, aux->priority, aux->desc, aux->date);
+            aux = aux->prox;
+        } while (aux != l.start);
+    }
+    fclose(file);
+}
+
+void loadFromFile(ListaCDE *l) {
+    FILE *file = fopen("todo.txt", "r");
+    if (file == NULL) {
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        int id, priority;
+        char desc[100], date[11];
+
+        line[strcspn(line, "\n")] = '\0';
+
+        char *token = strtok(line, ";");
+        if (token != NULL) id = atoi(token);
+
+        token = strtok(NULL, ";");
+        if (token != NULL) priority = atoi(token);
+
+        token = strtok(NULL, ";");
+        if (token != NULL) strcpy(desc, token);
+
+        token = strtok(NULL, ";");
+        if (token != NULL) strcpy(date, token);
+
+        insert(l, id, desc, priority, date);
+    }
+    fclose(file);
 }
 
 int main() {
     ListaCDE l;
-    int data, sucess, option;
+    int id, priority, success, option;
+    char desc[100], date[11];
 
     create(&l);
 
-    sucess = insert(&l, 1, "Teste 1", 1, "01/01/2021");
-    sucess = insert(&l, 2, "Teste 2", 1, "01/01/2021");
-    sucess = insert(&l, 3, "Teste 3", 1, "01/01/2021");
-    sucess = insert(&l, 4, "Teste 4", 1, "01/01/2021");
+    loadFromFile(&l);
 
     if(!isEmpty(l)) {
-        printf("Deseja ver os dados a partir de onde?\n");
-        printf("1 - start\n");
-        printf("2 - end\n");
-        printf("-> ");
-        scanf("%d", &option);
-
-        show(l, option);
+        printf("Tarefas existentes:\n");
+        show(l);
+    } else {
+        printf("Nenhuma tarefa existente.\n");
     }
 
-    option = 1;
-    while(option == 1) {
-        printf("Selecione o dado que deseja remover: ");
-        scanf("%d", &data);
+    do {
+        printf("\nSelecione uma opcao:\n");
+        printf("1 - Adicionar tarefa\n");
+        printf("2 - Remover tarefa\n");
+        printf("3 - Mostrar tarefas\n");
+        printf("0 - Sair\n");
+        printf("-> ");
+        scanf("%d", &option);
+        getchar();
 
-        sucess = removal(&l, data);
-        if(!sucess) {
-            printf("Erro ao remover o dado: %d\n", data);
+        switch(option) {
+            case 1:
+                printf("Digite o ID da tarefa: ");
+                scanf("%d", &id);
+                getchar();
+
+                printf("Digite a prioridade da tarefa (1: Alta, 2: Media, 3: Baixa): ");
+                scanf("%d", &priority);
+                getchar();
+
+                printf("Digite a descricao da tarefa: ");
+                fgets(desc, sizeof(desc), stdin);
+                desc[strcspn(desc, "\n")] = '\0';
+
+                printf("Digite a data de entrega da tarefa (dd/mm/aaaa): ");
+                fgets(date, sizeof(date), stdin);
+                date[strcspn(date, "\n")] = '\0';
+
+                success = insert(&l, id, desc, priority, date);
+
+                if(!success) {
+                    printf("Erro ao adicionar a tarefa: %d\n", id);
+                } else {
+                    saveToFile(l);
+                    printf("Tarefa adicionada com sucesso.\n");
+                }
+                break;
+
+            case 2:
+                if(isEmpty(l)) {
+                    printf("Nenhuma tarefa para remover.\n");
+                    break;
+                }
+                printf("Digite o ID da tarefa que deseja remover: ");
+                scanf("%d", &id);
+
+                success = removal(&l, id);
+                if(!success) {
+                    printf("Erro ao remover a tarefa: %d\n", id);
+                } else {
+                    saveToFile(l);
+                    printf("Tarefa removida com sucesso.\n");
+                }
+                break;
+
+            case 3:
+                if(isEmpty(l)) {
+                    printf("Nenhuma tarefa para mostrar.\n");
+                } else {
+                    show(l);
+                }
+                break;
+
+            case 0:
+                saveToFile(l);
+                printf("Saindo do programa.\n");
+                break;
+
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+                break;
         }
-        
-        show(l, 1);
-
-        printf("Deseja remover outro dado?\n");
-        printf("1 - Sim\n");
-        printf("2 - Nao\n");
-        printf("-> ");
-        scanf("%d", &option);
-    }
+    } while(option != 0);
 
     while(!isEmpty(l)) {
-        sucess = removal(&l, getFirst(l));
+        removal(&l, l.start->id);
     }
+
+    return 0;
 }
